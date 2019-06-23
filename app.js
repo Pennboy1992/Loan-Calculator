d4850a92661789db13881d903400f4df99b044de
Mad Marine <pnnroman92@gmail.com>
2019-06-23 18:37:55 -0500

    I put a loading.gif to make a better user experience and I also set it to hide the results and the loading.gif to not display when their values are empty and the loading.gif also displays even if user did not input any information before the error alert is dissplayed

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', caculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+
+  e.preventDefault();
+});
 
 // Calculate Resultss
-function caculateResults(e){
+function calculateResults(e){
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -25,17 +38,29 @@ function caculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+    
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
 
   } else{
     showError('Please check your numbers');
 
   }
 
-  e.preventDefault();
+ 
 }
 
 // Show Error
 function showError(error){
+// Hide results
+document.getElementById('results').style.display = 'none';
+
+// Hide loader
+document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
